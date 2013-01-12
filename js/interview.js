694f08b7776b0fcf8e4ee8caddd45f4297af5e2d
Alexander Ramirez <adr2370@gmail.com>
2013-01-11 16:46:34 -0800

    Moved tokbox stuff to separate file

---
+++ js/interview.js
@@ -0,0 +1,23 @@
+TB.addEventListener("exception", exceptionHandler);
+var session = TB.initSession(sessionId); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
+session.addEventListener("sessionConnected", sessionConnectedHandler);
+session.addEventListener("streamCreated", streamCreatedHandler);
+session.connect(apiKey, token); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
+function sessionConnectedHandler(event) {
+	 subscribeToStreams(event.streams);
+	 session.publish();
+}
+function streamCreatedHandler(event) {
+	subscribeToStreams(event.streams);
+}
+function subscribeToStreams(streams) {
+	for (i = 0; i < streams.length; i++) {
+		var stream = streams[i];
+		if (stream.connection.connectionId != session.connection.connectionId) {
+			session.subscribe(stream);
+		}
+	}
+}
+function exceptionHandler(event) {
+	console.log(event.message);
+}

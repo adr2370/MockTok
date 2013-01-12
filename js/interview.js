82795fdf34d79c3d05b6a61adb2381a40dd4ab12
Alexander Ramirez <adr2370@gmail.com>
2013-01-11 17:13:02 -0800

    Added streams container

--- js/interview.js
+++ js/interview.js
@@ -5,27 +5,39 @@ session.addEventListener("streamCreated", streamCreatedHandler);
 session.connect(apiKey, token); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
 function sessionConnectedHandler(event) {
 	 subscribeToStreams(event.streams);
-	var publisherContainer = document.getElementById('myPublisherDiv');
+	var div = document.createElement('div');
+	     div.setAttribute('id', 'publisher');
+
+	     var publisherContainer = document.getElementById('publisherContainer');
+	         // This example assumes that a publisherContainer div exists
+	     publisherContainer.appendChild(div);
+
 	     var publisherProperties = {width: 400, height:300, name:"Bob's stream"};
-	     publisher = TB.initPublisher(apiKey, 'publisher', publisherProperties);
+	     publisher = TB.initPublisher(API_KEY, 'publisher', publisherProperties);
 	     session.publish(publisher);
 }
-function sessionConnectHandler(event) {
-    var divProps = {width: 400, height:300, name:"Alex's stream"};
-    publisher = TB.initPublisher(apiKey, 'publisher', divProps);
-                      // This assumes that there is a DOM element with the ID 'publisher'.
-    session.publish(publisher);
-}
 function streamCreatedHandler(event) {
-	subscribeToStreams(event.streams);
+    for (var i = 0; i < event.streams.length; i++) {
+        var stream = event.streams[i];
+		if (stream.connection.connectionId != session.connection.connectionId) {
+			displayStream(stream);
+		}
+    }
 }
 function subscribeToStreams(streams) {
 	for (i = 0; i < streams.length; i++) {
 		var stream = streams[i];
 		if (stream.connection.connectionId != session.connection.connectionId) {
-			session.subscribe(stream);
+			displayStream(stream);
 		}
 	}
+}
+function displayStream(stream) {
+    var div = document.createElement('div');
+    div.setAttribute('id', 'stream' + stream.streamId);
+    var streamsContainer = document.getElementById('streamsContainer');
+    streamsContainer.appendChild(div);
+    subscriber = session.subscribe(stream, 'stream' + stream.streamId);
 }
 function exceptionHandler(event) {
 	console.log(event.message);
